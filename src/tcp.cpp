507adcfec801570e7fb6db9e7ac3387a807f0c46
jak <jackogina60@gmail.com>
2021-01-04 01:34:33 +0300

    tcp interface & impl

--- src/tcp.cpp
+++ src/tcp.cpp
@@ -1,17 +1,20 @@
 #include "common.h"
+#include <inttypes.h>
 #include "op_codes.h"
 #include "tcp.h"
 #include "WiFiClient.h"
+#include "Client.h"
 
 tcp_state_t tcp_state = TCP_STATE_IDLE;
 
 WiFiClient client;
 
 char host[MAX_IP_LEN + 1];
+uint16_t port = 0;
 
 uint8_t host_len = 0;
 
-void op_tcp_handle(uint8_t ins) {
+void op_tcp_state_machine(uint8_t ins) {
     uint8_t op = ins >> 4;
 
     switch (op) {
@@ -36,7 +39,7 @@ void op_tcp_handle(uint8_t ins) {
             break;
 
         case LNC_OP_TCP_AVAILABLE:
-            op_tcp_port_handle(ins);
+            op_tcp_available_handle(ins);
             break;
 
         case LNC_OP_TCP_SEND:
@@ -53,16 +56,24 @@ void op_tcp_handle(uint8_t ins) {
 }
 
 
+void op_tcp_start_handle(uint8_t ins) {
+    //should we check 
+    tcp_state = TCP_STATE_START;
+
+    Serial.write(LNC_ERROR_OK);
+
+}
+
 
 void op_tcp_host_handle(uint8_t ins) {
     if (tcp_state != TCP_STATE_START) {
-        Serial.write(LNC_ERROR_KO);
+        Serial.write(LNC_ERROR_INVALID_STATE);
         return;
     }
 
-    host_len = ins & 15;
+    host_len = ins & LEFT_4_BITS_MAP;
     if (host_len == 0 || host_len  > MAX_IP_LEN) {
-        Serial.write(LNC_ERROR_KO);
+        Serial.write(LNC_ERROR_INVALID_LEN);
         return;
     }
 
@@ -71,8 +82,8 @@ void op_tcp_host_handle(uint8_t ins) {
 
     uint8_t c = 0;
     while (c < host_len) {
-        if (millis() - timeout) {
-            Serial.write(LNC_ERROR_KO);
+        if (millis() - timeout >=  TCP_HOST_SEND_TIMEOUT) {
+            Serial.write(LNC_ERROR_TIMEOUT);
             return;
         }
         if (Serial.available()) {
@@ -84,4 +95,211 @@ void op_tcp_host_handle(uint8_t ins) {
 
     Serial.write(LNC_ERROR_OK);
 
+    tcp_state = TCP_STATE_HOST;
+
+}
+
+
+void op_tcp_port_handle(uint8_t ins){
+
+    if(tcp_state !=  TCP_STATE_HOST){
+        Serial.write(LNC_ERROR_INVALID_STATE);
+        return;
+    }
+
+    uint8_t port_len = ins & LEFT_4_BITS_MAP;
+
+    if(port_len  > MAX_PORT_LEN){
+        Serial.write(LNC_ERROR_INVALID_LEN);
+        return;
+    }
+
+    char port_c[port_len+1];
+
+    uint8_t count = 0;
+    unsigned long timeout = millis();
+    while(count < port_len){
+        if(millis() - timeout >=  TCP_PORT_SEND_TIMEOUT){
+            Serial.write(LNC_ERROR_TIMEOUT);
+            return;
+        }
+
+        if(Serial.available()){
+            port_c[count] = (char)Serial.read();
+            count++;
+        }
+    }
+
+
+    if(sscanf (port_c, "%" SCNd16 "", &port) <= 0){
+        Serial.write(LNC_ERROR_TCP_INVALID_PORT);
+        return;
+    }
+
+    if(port < MIN_PORT || port > MAX_PORT){
+        Serial.write(LNC_ERROR_TCP_INVALID_PORT);
+        return;
+    }
+
+    Serial.write(LNC_ERROR_OK);
+
+    tcp_state = TCP_STATE_HOST;
+
+}
+
+
+void op_tcp_connect_handle(uint8_t ins) {
+
+    if(tcp_state !=  TCP_STATE_PORT){
+        Serial.write(LNC_ERROR_INVALID_STATE);
+        return;
+    }
+
+    if(client.connect((const char*)host, port)){
+        Serial.write(LNC_ERROR_OK);
+        tcp_state = TCP_STATE_CONNECTED;
+        return;
+    }
+
+    Serial.write(LNC_ERROR_TCP_CONN);
+    
+}
+
+void op_tcp_available_handle(uint8_t ins){
+    if(tcp_state !=  TCP_STATE_CONNECTED){
+        Serial.write(LNC_ERROR_INVALID_STATE);
+        return;
+    }
+
+    if(!client.connected()){
+        Serial.write(LNC_ERROR_TCP_CONN);
+        return; 
+    }
+
+    int av = client.available();
+
+    if(av < 0){
+        Serial.write(LNC_ERROR_KO);
+        return;   
+    }
+
+    if(av > MAX_TCP_AVAILABLE){
+        Serial.write(MAX_TCP_AVAILABLE);
+        return;
+    }
+
+    Serial.write((uint8_t)av);
+
+}
+
+
+void op_tcp_send_handle(uint8_t ins){
+
+    if(tcp_state !=  TCP_STATE_CONNECTED){
+        Serial.write(LNC_ERROR_INVALID_STATE);
+        return;
+    }
+
+    if(!client.connected()){
+        Serial.write(LNC_ERROR_TCP_CONN);
+        return; 
+    }
+
+    uint8_t send_len = ins & LEFT_4_BITS_MAP;
+
+    if(send_len  > MAX_TCP_SEND){
+        Serial.write(LNC_ERROR_INVALID_LEN);
+        return;
+    }
+
+    char send_buffer[send_len+1];
+
+    uint8_t count = 0;
+    unsigned long timeout = millis();
+    while(count < send_len){
+        if(millis() - timeout >= TCP_SEND_TIMEOUT){
+            Serial.write(LNC_ERROR_TIMEOUT);
+            return;
+        }
+
+        if(Serial.available()){
+            send_buffer[count] = Serial.read();
+            count++;
+        }
+    }
+
+    size_t sent = client.write((const char*)send_buffer, (size_t)send_len);
+    
+    if(sent == 0 || sent > (size_t)send_len){
+        Serial.write(LNC_ERROR_TCP_SEND);
+        return;
+    }
+
+    Serial.write((uint8_t)sent);
+    return;
+
+}
+
+
+
+void op_tcp_recv_handle(uint8_t ins){
+
+    if(tcp_state !=  TCP_STATE_CONNECTED){
+        Serial.write(LNC_ERROR_INVALID_STATE);
+        return;
+    }
+
+    if(!client.connected()){
+        Serial.write(LNC_ERROR_TCP_CONN);
+        return; 
+    }
+
+    uint8_t read_len = ins & LEFT_4_BITS_MAP;
+
+    if(read_len  > MAX_TCP_AVAILABLE){
+        Serial.write(LNC_ERROR_INVALID_LEN);
+        return;
+    }
+
+    int av = client.available();
+    //no data to read
+    if(av <= 0){
+        Serial.write(0);
+        return;   
+    }
+
+    if(av > MAX_TCP_AVAILABLE){
+        av = MAX_TCP_AVAILABLE;
+    }
+
+    if(read_len > av){
+        read_len = av;
+    }
+
+    char recv_buffer[read_len+1];
+
+    int read = client.read((uint8_t*)recv_buffer, (size_t)read_len);
+    
+    if(read > read_len || read < 0){
+        Serial.write(LNC_ERROR_TCP_RECV);
+        return;
+    }
+
+    if((uint8_t)read !=  read_len){
+        read_len = (uint8_t)read;
+    }
+
+    Serial.write(read_len);
+    Serial.write((uint8_t*)recv_buffer, (size_t)read_len);
+    Serial.write(LNC_ERROR_OK);
+
+}
+
+void op_tcp_stop_handle(uint8_t ins){
+
+    client.stop();
+
+    tcp_state = TCP_STATE_START;
+
+    Serial.write(LNC_ERROR_OK);
 }
--- src/tcp.h
+++ src/tcp.h
@@ -3,12 +3,18 @@
 
 #include "common.h"
 
-#define HOST_SEND_TIMEOUT 1000
-#define PORT_SEND_TIMEOUT 1000
+#define TCP_HOST_SEND_TIMEOUT 1000
+#define TCP_PORT_SEND_TIMEOUT 1000
+#define TCP_SEND_TIMEOUT 1000
 
 #define MAX_IP_LEN 15
 #define MAX_PORT_LEN 5
+#define MIN_PORT 1
 #define MAX_PORT 65535
+
+#define MAX_TCP_AVAILABLE 200 //201 -254 reserved for error messages, 255 for polling
+#define MAX_TCP_SEND 15
+#define MAX_TCP_READ MAX_TCP_AVAILABLE
 
 typedef enum tcp_states {
     TCP_STATE_IDLE = 0, //not connected
@@ -21,44 +27,62 @@ typedef enum tcp_states {
 /**
  * handles TCP op codes
  */
-void op_tcp_handle(uint8_t ins);
+void op_tcp_state_machine(uint8_t ins);
 
 /**
  * starts a TCP session
+ * on success LNC_ERROR_OK is written
+ * on error appropriate error is written
  */
 void op_tcp_start_handle(uint8_t ins);
 
 /**
  * sets TCP session host
+ * on success LNC_ERROR_OK is written
+ * on error appropriate error is written
  */
 void op_tcp_host_handle(uint8_t ins);
 
 /**
  * sets TCP port
+ * on success LNC_ERROR_OK is written
+ * on error appropriate error is written
  */
 void op_tcp_port_handle(uint8_t ins);
 
 /**
  * connects to TCP server
+ * on success LNC_ERROR_OK is written
+ * on error appropriate error is written
  */
 void op_tcp_connect_handle(uint8_t ins);
 
 /**
- * sends data to connected tcp server
- * can send a maximum of 255 bytes per call
- */
-void op_tcp_send_handle(uint8_t ins);
-
-/**
  * checks if theres any data to read from server
- * returns a maximum of 254
+ * on success number of available bytes to read is written
+ * returns a maximum of MAX_TCP_AVAILABLE
+ * on error appropriate error is written
  */
 void op_tcp_available_handle(uint8_t ins);
 
 /**
+ * sends data to connected tcp server
+ * can send a maximum of MAX_TCP_SEND bytes per call
+ * on success writes back the number of bytes sent
+ * on error appropriate error is written
+ */
+void op_tcp_send_handle(uint8_t ins);
+
+
+
+/**
  * receives data from a tcp server
- * can receive a maximum of 254 bytes per call
- *
+ * can receive a maximum of MAX_TCP_AVAILABLE bytes per call
+ * make sure to check number of bytes available before calling
+ * will first send number of bytes available to writing before writing
+ * the actual bytes and terminating with a LNC_ERROR_OK
+ * if not data is available for receiving it will send a 0 and exit
+ * if an error occurs during reading it will send the error instead
  */
 void op_tcp_recv_handle(uint8_t ins);
 
