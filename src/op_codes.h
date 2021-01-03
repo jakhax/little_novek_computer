9efa9f3c8ead99cbbfdddf02c84063e0594b5f1d
jak <jackogina60@gmail.com>
2021-01-04 01:34:12 +0300

    added error codes

--- src/op_codes.h
+++ src/op_codes.h
@@ -3,10 +3,11 @@
 
 #include "common.h"
 
+#define LEFT_4_BITS_MAP 15
+
 typedef enum lnc_ops {
     LNC_OP_GPIO_OUT = 0,
     LNC_OP_GPIO_IN,
-    LNC_OP_SLEEP,
     LNC_OP_TCP_START,
     LNC_OP_TCP_STOP,
     LNC_OP_TCP_HOST,
@@ -15,13 +16,43 @@ typedef enum lnc_ops {
     LNC_OP_TCP_SEND,
     LNC_OP_TCP_AVAILABLE,
     LNC_OP_TCP_RECV,
-    LNC_OP_POLL,
+    //used to poll the connected device for instructions
+    LNC_OP_POLL = 255,
 
 } lnc_ops_t;
 
+//return values 201 -254 reserved for error messages
+
 typedef enum lnc_op_err {
+    //no error
     LNC_ERROR_OK = 0,
-    LNC_ERROR_KO = 255,
+    //invalid input
+    LNC_ERROR_INVALID_INPUT = 201,
+    //machine not in expected state
+    LNC_ERROR_INVALID_STATE,
+    //invalid gpio pin
+    LNC_ERROR_INVALID_GPIO_PIN,
+    //invalid gpio state
+    LNC_ERROR_INVALID_GPIO_STATE,
+    //gpio OUT error
+    LNC_ERROR_GPIO_OUT,
+    //gpio IN read error
+    LNC_ERROR_GPIO_IN,
+    //tcp conn error (also used when client is not connected)
+    LNC_ERROR_TCP_CONN,
+    //tcp send error
+    LNC_ERROR_TCP_SEND,
+    //tcp recv error
+    LNC_ERROR_TCP_RECV,
+    //invalid port
+    LNC_ERROR_TCP_INVALID_PORT,
+    //uart read timeout
+    LNC_ERROR_TIMEOUT,
+    //sent wrong len
+    LNC_ERROR_INVALID_LEN,
+
+    //unknown error
+    LNC_ERROR_KO = 254,
 
 } lnc_error_t;
 
